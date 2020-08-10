19b95cde8f3f7ad16f3121caf4eaeb909b706d4f
DinethP <dineth_perera@hotmail.com>
2020-08-10 23:48:41 +0530

    Adding Header links

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,10 @@ import SearchIcon from "@material-ui/icons/Search";
 function Header() {
   return (
     <nav className="header">
-      <Link to="/">   {/*Link back to homepage */}
+      {/* Link is better than href tag bevause it doesnt reload the page */}
+      <Link to="/">
+        {" "}
+        {/*Link back to homepage */}
         {/*logo on the left -> img  */}
         <img
           className="header__logo"
@@ -19,7 +22,33 @@ function Header() {
         <input className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
       </div>
+
       {/* 3 links (sign-in/orders/prime)*/}
+      <div className="header__nav">
+        {/*Link 1: sign-in */}
+        <Link to="/login" className="header__link">
+          <div className="header__options">
+            <span>Hello Dineth</span>
+            <span>Sign In</span>
+          </div>
+        </Link>
+
+        {/*Link 2: orders*/}
+        <Link to="/" className="header__link">
+          <div className="header__options">
+            <span>Returns</span>
+            <span>Orders</span>
+          </div>
+        </Link>
+
+        {/*Link 3: prime*/}
+        <Link to="/" className="header__link">
+          <div className="header__options">
+            <span>Your</span>
+            <span>Prime</span>
+          </div>
+        </Link>
+      </div>
       {/* basket icon with number */}
     </nav>
   );
